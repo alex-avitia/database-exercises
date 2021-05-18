58154af22df3f24d534aee97f218ba2acf08f78e
Alex Avitia <alex.avitia77@gmail.com>
2021-05-18 12:38:16 -0500

    Finished: added albums database

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+USE codeup_test_db;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) DEFAULT 'UNKNOWN',
+    name VARCHAR(100) DEFAULT 'UNKNOWN',
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY(id)
+);
+
+SELECT * FROM albums;
